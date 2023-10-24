03018ace5056a3f30704e1c2a34309c610560b97
brislazaro <brisa.lazaroc@gmail.com>
2023-10-24 17:23:45 +0200

    game of life

---
+++ gameOfLife/prueba1.js
@@ -0,0 +1,97 @@
+const board = document.querySelector(".container");
+
+let firstGame = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+let nextGeneration = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+const cellAlive = 1;
+const cellDied = 0;
+
+function printTable(table) {
+  for (let i = 0; i < table.length; i++) {
+    const currentRow = table[i];
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    board.appendChild(row);
+
+    for (let j = 0; j < currentRow.length; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("celda");
+
+      if (table[i][j] === cellAlive) {
+        cell.classList.add("celda--alive");
+      }
+
+      // cell.addEventListener("click", () => {
+      // });
+
+      row.appendChild(cell);
+    }
+  }
+}
+
+printTable(firstGame);
+
+function contarVecinos(row, col) {
+  let totalVecinos = 0;
+
+  const previous = firstGame[row - 1] || [];
+  const current = firstGame[row];
+  const next = firstGame[row + 1] || [];
+
+  totalVecinos += previous[col - 1] || 0;
+  totalVecinos += previous[col] || 0;
+  totalVecinos += previous[col + 1] || 0;
+
+  totalVecinos += current[col - 1] || 0;
+  totalVecinos += current[col + 1] || 0;
+
+  totalVecinos += next[col - 1] || 0;
+  totalVecinos += next[col] || 0;
+  totalVecinos += next[col + 1] || 0;
+
+  return totalVecinos;
+}
+
+function applyRulesToCell(row, col) {
+  const vecinosVivos = contarVecinos(row, col);
+  const celdaActual = firstGame[row][col];
+
+  let nextCellState = 0;
+
+  if ((vecinosVivos === 2 || vecinosVivos === 3) && celdaActual === 1) {
+    nextCellState = 1;
+  }
+
+  if (vecinosVivos === 3 && celdaActual === 0) {
+    nextCellState = 1;
+  }
+
+  nextGeneration[row][col] = nextCellState;
+}
+
+function calculateNextGeneration() {
+  for (let i = 0; i < firstGame.length; i++) {
+    const element = firstGame[i];
+
+    for (let j = 0; j < element.length; j++) {
+      applyRulesToCell(i, j);
+    }
+  }
+
+  console.log(nextGeneration);
+}
+
+calculateNextGeneration();
