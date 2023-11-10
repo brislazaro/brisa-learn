541e6571464aa01fd935c580e4a5d296ca5845c5
brislazaro <brisa.lazaroc@gmail.com>
2023-11-10 10:08:57 +0100

    list restaurants

---
+++ bcn/data.js
@@ -0,0 +1,17 @@
+const restaurants = [
+  {
+    nombre: "Tamarindo",
+    logo: "../img/tamarindo.png",
+    type: "Mexican",
+  },
+  {
+    nombre: "La curandera",
+    logo: "../img/curandera.jpeg",
+    type: "Mexican",
+  },
+  {
+    nombre: "Harrys",
+    logo: "../img/harrys.png",
+    type: "Italian",
+  },
+];
--- bcn/restaurants/restaurants.js
+++ bcn/restaurants/restaurants.js
@@ -10,15 +10,59 @@ function printTitle(selectedRestaurant) {
 
 printTitle();
 
-const stars = document.querySelectorAll(".star");
+// filtrar los restaurantes segun el tipo
+const filteredRestaurants = restaurants.filter(
+  (restaurant) => restaurant.type === selectedRestaurant
+);
 
-stars.forEach(function (star, index) {
-  star.addEventListener("click", function () {
-    for (let i = 0; i <= index; i++) {
-      stars[i].classList.add("checked");
+function printRestaurants() {
+  for (let i = 0; i < filteredRestaurants.length; i++) {
+    const element = filteredRestaurants[i];
+
+    const cardRest = document.querySelector(".list-container");
+
+    const containerEl = document.createElement("li");
+    containerEl.classList.add("container-restaurant");
+    cardRest.appendChild(containerEl);
+
+    const logoEl = document.createElement("img");
+    logoEl.classList.add("logo");
+    logoEl.src = element.logo;
+    logoEl.alt = "hola";
+
+    containerEl.appendChild(logoEl);
+
+    const nameEl = document.createElement("p");
+    nameEl.innerHTML = element.nombre;
+
+    containerEl.appendChild(nameEl);
+
+    const typeEl = document.createElement("p");
+    typeEl.innerHTML = element.type;
+
+    containerEl.appendChild(typeEl);
+
+    const starsContainerEl = document.createElement("div");
+    starsContainerEl.classList.add("rating");
+
+    containerEl.appendChild(starsContainerEl);
+
+    for (let index = 0; index < 5; index++) {
+      const starEl = document.createElement("i");
+      starEl.classList.add("star", "bi", "bi-star-fill");
+
+      starEl.addEventListener("click", () => {
+        for (let i = 0; i <= index; i++) {
+          starsContainerEl.children[i].classList.add("checked");
+        }
+        for (let i = index + 1; i < starsContainerEl.children.length; i++) {
+          starsContainerEl.children[i].classList.remove("checked");
+        }
+      });
+
+      starsContainerEl.appendChild(starEl);
     }
-    for (let i = index + 1; i < stars.length; i++) {
-      stars[i].classList.remove("checked");
-    }
-  });
-});
+  }
+}
+
+printRestaurants();
