9c57318fe9196b074e74e7247e4badf9f4d4ff64
brislazaro <brisa.lazaroc@gmail.com>
2023-11-15 12:45:35 +0100

    final seccion11

---
+++ curso/11-Iteradores/js/07-app.js
@@ -0,0 +1,14 @@
+const pendientes = ["tarea", "comer", "proyecto", "estudiar js"];
+
+const carrito = [
+  { nombre: "monitor 27 pulgadas", precio: 500 },
+  { nombre: "television", precio: 100 },
+  { nombre: "tablet", precio: 200 },
+  { nombre: "audifonos", precio: 300 },
+  { nombre: "teclado", precio: 400 },
+  { nombre: "celular", precio: 700 },
+];
+
+for (let pendiente of pendientes) {
+  console.log(pendiente);
+}
---
+++ curso/11-Iteradores/js/08-app.js
@@ -0,0 +1,5 @@
+const pendientes = ["tarea", "comer", "proyecto", "estudiar js"];
+
+for (let pendiente in pendientes) {
+  console.log(pendiente);
+}
