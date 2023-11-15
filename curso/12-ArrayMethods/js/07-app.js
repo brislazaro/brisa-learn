7506b5f3e9bbc8a2523e6e7bab3f653df90de702
brislazaro <brisa.lazaroc@gmail.com>
2023-11-15 13:39:25 +0100

    seccion12 part2

---
+++ curso/12-ArrayMethods/js/07-app.js
@@ -0,0 +1,5 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"];
+const meses2 = ["Agosto", "Septiembre"];
+
+const meses3 = meses.concat(meses2);
+console.log(meses3);
