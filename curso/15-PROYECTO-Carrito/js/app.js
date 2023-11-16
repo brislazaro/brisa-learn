a8ece8091034c7096b6d37f0634783e4ebc84c8d
brislazaro <brisa.lazaroc@gmail.com>
2023-11-16 11:10:55 +0100

    part1

---
+++ curso/15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,16 @@
+const carrito = document.querySelector("#carrito");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
+const vaciarCarritoBtn = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+
+cargarEventListener();
+function cargarEventListener() {
+  listaCursos.addEventListener("click", agregarCurso);
+}
+
+function agregarCurso(e) {
+  e.preventDefault();
+  if (e.target.classList.contains("agregar-carrito")) {
+    console.log("si");
+  }
+}
