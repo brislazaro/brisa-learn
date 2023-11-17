b242df3f40f277438de51adec72afab6e9a205e7
brislazaro <brisa.lazaroc@gmail.com>
2023-11-17 12:39:40 +0100

    validacion de campos

---
+++ curso/16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
+
+  inputEmail.addEventListener("blur", validar);
+
+  inputAsunto.addEventListener("blur", validar);
+
+  inputMensaje.addEventListener("blur", validar);
+
+  function validar(e) {
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+  }
+
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
+    const error = document.createElement("p");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2");
+
+    referencia.appendChild(error);
+  }
+});
+
+function limpiarAlerta(referencia) {
+  const alerta = referencia.querySelector(".bg-red-600");
+  if (alerta) {
+    alerta.remove();
+  }
+}
