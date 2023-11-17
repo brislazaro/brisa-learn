ce7d1af7d1f196e3f271266cd78d602cd8e8ef6a
brislazaro <brisa.lazaroc@gmail.com>
2023-11-17 17:37:29 +0100

    reto

--- curso/16-PROYECTO-EnviarEmail/js/app.js
+++ curso/16-PROYECTO-EnviarEmail/js/app.js
@@ -10,13 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputMensaje = document.querySelector("#mensaje");
   const formulario = document.querySelector("#formulario");
   const btnSubmit = document.querySelector('#formulario button[type="submit"]');
-  const btnReset = document.querySelector('#formulario button[type="submit"]');
+  const btnReset = document.querySelector('#formulario button[type="reset"]');
+  const spinner = document.querySelector("#spinner");
+  const emailContainer = document.querySelector("#div");
 
   inputEmail.addEventListener("input", validar);
 
   inputAsunto.addEventListener("input", validar);
 
   inputMensaje.addEventListener("input", validar);
+
+  formulario.addEventListener("submit", enviarEmail);
 
   btnReset.addEventListener("click", function (e) {
     e.preventDefault();
@@ -25,8 +29,48 @@ document.addEventListener("DOMContentLoaded", function () {
     email.asunto = "";
     email.mensaje = "";
 
-    formulario.remove();
+    formulario.reset();
+    comprobarEmail();
   });
+
+  function enviarEmail(e) {
+    e.preventDefault();
+
+    spinner.classList.add("flex");
+    spinner.classList.remove("hidden");
+
+    setTimeout(() => {
+      spinner.classList.remove("flex");
+      spinner.classList.add("hidden");
+
+      email.email = "";
+      email.asunto = "";
+      email.mensaje = "";
+
+      formulario.reset();
+      comprobarEmail();
+
+      const alertaExito = document.createElement("p");
+      alertaExito.classList.add(
+        "bg-green-500",
+        "text-white",
+        "p-2",
+        "text-center",
+        "rounded-lg",
+        "mt-10",
+        "font-bold",
+        "text-sm",
+        "uppercase"
+      );
+
+      alertaExito.textContent = "Mensaje enviado correctamente";
+      formulario.appendChild(alertaExito);
+
+      setTimeout(() => {
+        alertaExito.remove();
+      }, 3000);
+    }, 3000);
+  }
 
   function validar(e) {
     if (e.target.value.trim() === "") {
@@ -85,4 +129,34 @@ document.addEventListener("DOMContentLoaded", function () {
       btnSubmit.disabled = false;
     }
   }
+
+  const label = document.createElement("label");
+  label.classList.add("font-regular", "font-medium");
+  label.innerHTML = "CC";
+  emailContainer.appendChild(label);
+
+  const campoExtra = document.createElement("input");
+  campoExtra.type = "email";
+  campoExtra.classList.add(
+    "border",
+    "border-gray-300",
+    "px-3",
+    "py-2",
+    "rounded-lg"
+  );
+
+  campoExtra.name = "C.C";
+
+  campoExtra.placeholder = "Email: Campo no obligatorio";
+
+  emailContainer.appendChild(campoExtra);
+
+  campoExtra.addEventListener("input", (e) => {
+    if (!validarEmail(e.target.value)) {
+      mostrarAlerta("El email no es valido", e.target.parentElement);
+      return;
+    }
+
+    limpiarAlerta(e.target.parentElement);
+  });
 });
