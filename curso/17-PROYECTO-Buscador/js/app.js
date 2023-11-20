1bcc351dba394c4e88bc6e526d476178f9aaead2
brislazaro <brisa.lazaroc@gmail.com>
2023-11-20 10:24:55 +0100

    lista de coches

---
+++ curso/17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,15 @@
+const resultado = document.querySelector("#resultado");
+
+document.addEventListener("DOMContentLoaded", () => {
+  mostrarAutos();
+});
+
+function mostrarAutos() {
+  autos.forEach((auto) => {
+    const autoHTML = document.createElement("p");
+    autoHTML.textContent = `
+    ${auto.marca} ${auto.modelo}-${auto.year}-${auto.puertas}Puertas - Transmision:${auto.transmision}- Precio:${auto.precio}- Color:${auto.color}
+    `;
+    resultado.appendChild(autoHTML);
+  });
+}
