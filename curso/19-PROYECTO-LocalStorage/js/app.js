fe5f0ddd74ad3a554475a5bd95776bbd31a1efc8
brislazaro <brisa.lazaroc@gmail.com>
2023-11-22 12:37:43 +0100

    part 1

---
+++ curso/19-PROYECTO-LocalStorage/js/app.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector("#formulario");
+const listaTweets = document.querySelector("#lista-tweets");
+let tweets = [];
+
+function eventListener() {
+  eventListener();
+  formulario.addEventListener("submit", agregarTweet);
+}
