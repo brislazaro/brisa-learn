31ec5947ec0e70ed38262d3ea85980cde1664d4a
brislazaro <brisa.lazaroc@gmail.com>
2023-11-22 12:54:40 +0100

    mensaje de error

--- curso/19-PROYECTO-LocalStorage/js/app.js
+++ curso/19-PROYECTO-LocalStorage/js/app.js
@@ -2,7 +2,31 @@ const formulario = document.querySelector("#formulario");
 const listaTweets = document.querySelector("#lista-tweets");
 let tweets = [];
 
+eventListener();
 function eventListener() {
-  eventListener();
   formulario.addEventListener("submit", agregarTweet);
+}
+
+function agregarTweet(e) {
+  e.preventDefault();
+
+  const tweet = document.querySelector("#tweet").value;
+
+  if (tweet === "") {
+    mostrarError("mensaje vacio");
+    return;
+  }
+}
+
+function mostrarError(error) {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = error;
+  mensajeError.classList.add("error");
+
+  const contenido = document.querySelector("#contenido");
+  contenido.appendChild(mensajeError);
+
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 3000);
 }
