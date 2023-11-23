8fdfbed4ec2066e61dd9f35ea4c6636dd9a2936e
brislazaro <brisa.lazaroc@gmail.com>
2023-11-23 16:45:32 +0100

    validacion

--- curso/23-PROYECTO-Prototypes-Seguro/js/app.js
+++ curso/23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -1,4 +1,4 @@
-function seguro(marca, year, tipo) {
+function Seguro(marca, year, tipo) {
   this.marca = marca;
   this.year = year;
   this.tipo = tipo;
@@ -7,12 +7,12 @@ function seguro(marca, year, tipo) {
 function UI() {}
 
 UI.prototype.llenarOpciones = () => {
-  const max = new Date().getFullYear(),
-    min = max - 20;
+  const max = new Date().getFullYear();
+  const min = max - 20;
 
   const selectYear = document.querySelector("#year");
 
-  for (let i = max; i > max; i++) {
+  for (let i = max; i > min; i--) {
     let option = document.createElement("option");
     option.value = i;
     option.textContent = i;
@@ -25,3 +25,25 @@ const ui = new UI();
 document.addEventListener("DOMContentLoaded", () => {
   ui.llenarOpciones();
 });
+
+eventListeners();
+function eventListeners() {
+  const formulario = document.querySelector("#cotizar-seguro");
+  formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+  e.preventDefault();
+
+  const marca = document.querySelector("#marca").value;
+
+  const year = document.querySelector("#yaer").value;
+
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  if (marca === "" || year === "" || tipo === "") {
+    console.log("no");
+  } else {
+    console.log("si");
+  }
+}
