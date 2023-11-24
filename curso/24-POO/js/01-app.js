4639b3e7b8fa2761d3e28bbbd4b2b2c6cca7bdfc
brislazaro <brisa.lazaroc@gmail.com>
2023-11-24 12:42:26 +0100

    video2

--- curso/24-POO/js/01-app.js
+++ curso/24-POO/js/01-app.js
@@ -3,9 +3,19 @@ class Cliente {
     this.nombre = nombre;
     this.saldo = saldo;
   }
+
+  mostrarInformacion() {
+    return `Cliente: ${this.nombre} tu saldo es de: ${this.saldo}`;
+  }
+
+  static bienvenida() {
+    return `Bienvenido al cajero`;
+  }
 }
 
-const brisa = new Cliente("juan", 400);
+const brisa = new Cliente("brisa", 400);
+console.log(brisa.mostrarInformacion);
 console.log("brisa");
+console.log(Cliente.bienvenida);
 
 const Cliente2 = class {};
