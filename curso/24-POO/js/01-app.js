74df4c272d8011f923ea36c29a41aedc7852c5c0
brislazaro <brisa.lazaroc@gmail.com>
2023-11-24 12:37:23 +0100

    app1

---
+++ curso/24-POO/js/01-app.js
@@ -0,0 +1,11 @@
+class Cliente {
+  constructor(nombre, saldo) {
+    this.nombre = nombre;
+    this.saldo = saldo;
+  }
+}
+
+const brisa = new Cliente("juan", 400);
+console.log("brisa");
+
+const Cliente2 = class {};
