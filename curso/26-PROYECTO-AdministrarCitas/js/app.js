5f03de046a8d20e8b17db658bf6666baa6859076
brislazaro <brisa.lazaroc@gmail.com>
2023-11-29 11:49:19 +0100

    validaciones

--- curso/26-PROYECTO-AdministrarCitas/js/app.js
+++ curso/26-PROYECTO-AdministrarCitas/js/app.js
@@ -7,6 +7,38 @@ const sintomasInput = document.querySelector("#sintomas");
 const formulario = document.querySelector("#nueva-cita");
 const contenedorCitas = document.querySelector("#citas");
 
+class Citas {
+  constructor() {
+    this.citas = [];
+  }
+}
+
+class UI {
+  imprimirAlerta(mensaje, tipo) {
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add("text-center", "alert", "d-block", "col-12");
+
+    if (tipo === "error") {
+      divMensaje.classList.add("alert-danger");
+    } else {
+      divMensaje.classList.add("alert-success");
+    }
+
+    divMensaje.textContent = mensaje;
+
+    document
+      .querySelector("#contenido")
+      .insertBefore(divMensaje, document.querySelector(".agregar-cita"));
+
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 5000);
+  }
+}
+
+const ui = new UI();
+const administrarCitas = new Citas();
+
 eventListeners();
 function eventListeners() {
   mascotaInput.addEventListener("input", datosCita);
@@ -15,6 +47,7 @@ function eventListeners() {
   fechaInput.addEventListener("input", datosCita);
   horaInput.addEventListener("input", datosCita);
   sintomasInput.addEventListener("input", datosCita);
+  formulario.addEventListener("submit", nuevaCita);
 }
 
 const citaObj = {
@@ -28,4 +61,21 @@ const citaObj = {
 
 function datosCita(e) {
   citaObj[e.target.name] = e.target.value;
+}
+
+function nuevaCita(e) {
+  e.preventDefault();
+  const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+  if (
+    mascota === "" ||
+    propietario === "" ||
+    telefono === "" ||
+    fecha === "" ||
+    hora === "" ||
+    sintomas === ""
+  ) {
+    ui.imprimirAlerta("Todos los campos son obligatorios", "error");
+    return;
+  }
 }
