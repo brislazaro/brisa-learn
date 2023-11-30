05b7cd540dfc265f299524d2cec768e77f2be8fe
brislazaro <brisa.lazaroc@gmail.com>
2023-11-30 16:59:18 +0100

    creando tablas

--- curso/30-IndexedDB/js/app.js
+++ curso/30-IndexedDB/js/app.js
@@ -13,7 +13,16 @@ function crmDB() {
     console.log("base de datos creada");
   };
 
-  crmDB.onupgradeneeded = function () {
-    console.log("Este metodo solo se ejecuta una vez");
+  crmDB.onupgradeneeded = function (e) {
+    const db = e.target.result;
+
+    const objectStore = db.createObjectStore("crm", {
+      keyPath: "crm",
+      autoIncrement: true,
+    });
+
+    objectStore.createIndex("nombre", "nombre", { unique: false });
+    objectStore.createIndex("email", "email", { unique: true });
+    objectStore.createIndex("telefono", "telefono", { unique: false });
   };
 }
