600a9dd6d62a0793ae434816960af66ca52e8da2
brislazaro <brisa.lazaroc@gmail.com>
2023-12-06 11:45:41 +0100

    spinner

--- curso/36-PROYECTO-Clima/js/app.js
+++ curso/36-PROYECTO-Clima/js/app.js
@@ -54,6 +54,8 @@ function consultarAPI(ciudad, pais) {
 
   const coordinatesUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&appid=${appID}`;
 
+  spinner();
+
   fetch(coordinatesUrl)
     .then((res) => res.json())
     .then((data) => {
@@ -114,4 +116,29 @@ function limpiarHTML() {
   while (resultado.firstChild) {
     resultado.removeChild(resultado.firstChild);
   }
+}
+
+function spinner() {
+  limpiarHTML();
+  const divSpinner = document.createElement("div");
+  divSpinner.classList.add("sk-fading-circle");
+
+  divSpinner.innerHTML = `
+    
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle9 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
+
+    `;
+
+  resultado.appendChild(divSpinner);
 }
