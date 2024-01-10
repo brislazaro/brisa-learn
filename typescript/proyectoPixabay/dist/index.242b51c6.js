8018b8eb797d3a455310b13df72a869064812828
brislazaro <brisa.lazaroc@gmail.com>
2024-01-10 17:14:22 +0100

    proyect

--- typescript/proyectoPixabay/dist/index.242b51c6.js
+++ typescript/proyectoPixabay/dist/index.242b51c6.js
@@ -1,25 +1,59 @@
 document.addEventListener("DOMContentLoaded", function() {
     const busqueda = document.querySelector("#termino");
     const buscarBtn = document.querySelector("#button");
-    const result = document.querySelector("#resultado");
-    const apiKey = "41349536-034eb62f6662da54e42d5e1d7";
-    let valor = busqueda.value;
-    function printResult() {
-        if (valor === "") alert("A\xf1ade un termino de busqueda");
-        else {
-            valor = valor.replaceAll(" ", "+");
-            const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${valor}&image_type=photo`;
+    let result = document.querySelector("#resultado");
+    async function printResult() {
+        let valor = busqueda.value;
+        if (valor === "") {
+            const errorCard = document.createElement("div");
+            errorCard.classList.add("errorCard");
+            result?.appendChild(errorCard);
+            const errorText = document.createElement("p");
+            errorText.classList.add("errorText");
+            errorText.innerHTML = "A\xf1ade el nombre de un Pokemon";
+            errorCard.appendChild(errorText);
+        } else try {
+            createSpinner();
+            const apiUrl = `https://pokeapi.co/api/v2/pokemon/${valor}`;
+            let response = await fetch(apiUrl);
+            let json = await response.json();
             const card = document.createElement("div");
+            card.classList.add("resultCard");
             result?.appendChild(card);
-            const resultPrint = document.createElement("p");
-            resultPrint.innerHTML = busqueda.value;
-            card.appendChild(resultPrint);
+            const img = document.createElement("img");
+            img.classList.add("cardImg");
+            img.src = json.sprites.front_default;
+            card.appendChild(img);
+            const name = document.createElement("p");
+            name.classList.add("cardName");
+            name.innerHTML = json.name;
+            card.appendChild(name);
+        } catch (error) {
+            const errorCatch = document.createElement("div");
+            errorCatch.classList.add("errorCard");
+            result?.appendChild(errorCatch);
+            const errorTextCatch = document.createElement("p");
+            errorTextCatch.classList.add("errorText");
+            errorTextCatch.innerHTML = "A\xf1ade un nombre valido";
+            errorCatch.appendChild(errorTextCatch);
+        } finally{
+            removeSpinner();
         }
     }
     buscarBtn?.addEventListener("click", function(e) {
+        if (result) result.innerHTML = "";
         e.preventDefault();
         printResult();
     });
+    function createSpinner() {
+        const spinner = document.createElement("div");
+        spinner.classList.add("spinner");
+        result?.appendChild(spinner);
+    }
 });
+function removeSpinner() {
+    const spinner = document.querySelector(".spinner");
+    spinner?.remove();
+}
 
 //# sourceMappingURL=index.242b51c6.js.map
--- typescript/proyectoPixabay/index.ts
+++ typescript/proyectoPixabay/index.ts
@@ -1,28 +1,80 @@
 document.addEventListener("DOMContentLoaded", function () {
   const busqueda = document.querySelector("#termino") as HTMLInputElement;
   const buscarBtn = document.querySelector("#button");
-  const result = document.querySelector("#resultado");
+  let result = document.querySelector("#resultado");
 
-  const apiKey = "41349536-034eb62f6662da54e42d5e1d7";
-  let valor = busqueda.value;
+  type Pokemon = {
+    name: string;
+    sprites: {
+      front_default: string;
+    };
+  };
 
-  function printResult() {
+  async function printResult() {
+    let valor = busqueda.value;
+
     if (valor === "") {
-      alert("Añade un termino de busqueda");
-    } else {
-      valor = valor.replaceAll(" ", "+");
-      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${valor}&image_type=photo`;
-      const card = document.createElement("div");
-      result?.appendChild(card);
+      const errorCard = document.createElement("div");
+      errorCard.classList.add("errorCard");
+      result?.appendChild(errorCard);
 
-      const resultPrint = document.createElement("p");
-      resultPrint.innerHTML = busqueda.value;
-      card.appendChild(resultPrint);
+      const errorText = document.createElement("p");
+      errorText.classList.add("errorText");
+      errorText.innerHTML = "Añade el nombre de un Pokemon";
+      errorCard.appendChild(errorText);
+    } else {
+      try {
+        createSpinner();
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${valor}`;
+
+        let response = await fetch(apiUrl);
+        let json = (await response.json()) as Pokemon;
+
+        const card = document.createElement("div");
+        card.classList.add("resultCard");
+        result?.appendChild(card);
+
+        const img = document.createElement("img");
+        img.classList.add("cardImg");
+        img.src = json.sprites.front_default;
+        card.appendChild(img);
+
+        const name = document.createElement("p");
+        name.classList.add("cardName");
+        name.innerHTML = json.name;
+        card.appendChild(name);
+      } catch (error) {
+        const errorCatch = document.createElement("div");
+        errorCatch.classList.add("errorCard");
+        result?.appendChild(errorCatch);
+
+        const errorTextCatch = document.createElement("p");
+        errorTextCatch.classList.add("errorText");
+        errorTextCatch.innerHTML = "Añade un nombre valido";
+        errorCatch.appendChild(errorTextCatch);
+      } finally {
+        removeSpinner();
+      }
     }
   }
 
   buscarBtn?.addEventListener("click", function (e) {
+    if (result) {
+      result.innerHTML = "";
+    }
+
     e.preventDefault();
     printResult();
   });
+
+  function createSpinner() {
+    const spinner = document.createElement("div");
+    spinner.classList.add("spinner");
+    result?.appendChild(spinner);
+  }
 });
+
+function removeSpinner() {
+  const spinner = document.querySelector(".spinner");
+  spinner?.remove();
+}
