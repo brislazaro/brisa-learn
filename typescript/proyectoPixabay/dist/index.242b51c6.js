c10d52b15291cfe068d594f9df2b028df8e89967
brislazaro <brisa.lazaroc@gmail.com>
2024-01-11 11:42:30 +0100

    Remove .parcel-cache

--- typescript/proyectoPixabay/dist/index.242b51c6.js
+++ typescript/proyectoPixabay/dist/index.242b51c6.js
@@ -2,40 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const busqueda = document.querySelector("#termino");
     const buscarBtn = document.querySelector("#button");
     let result = document.querySelector("#resultado");
+    async function fetchPokemon(pokemonName) {
+        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+        let response = await fetch(apiUrl);
+        let json = await response.json();
+        return json;
+    }
     async function printResult() {
         let valor = busqueda.value;
-        if (valor === "") {
-            const errorCard = document.createElement("div");
-            errorCard.classList.add("errorCard");
-            result?.appendChild(errorCard);
-            const errorText = document.createElement("p");
-            errorText.classList.add("errorText");
-            errorText.innerHTML = "A\xf1ade el nombre de un Pokemon";
-            errorCard.appendChild(errorText);
-        } else try {
+        if (valor === "") createErrorCard("Introduce un pokemon");
+        else try {
             createSpinner();
-            const apiUrl = `https://pokeapi.co/api/v2/pokemon/${valor}`;
-            let response = await fetch(apiUrl);
-            let json = await response.json();
-            const card = document.createElement("div");
-            card.classList.add("resultCard");
-            result?.appendChild(card);
-            const img = document.createElement("img");
-            img.classList.add("cardImg");
-            img.src = json.sprites.front_default;
-            card.appendChild(img);
-            const name = document.createElement("p");
-            name.classList.add("cardName");
-            name.innerHTML = json.name;
-            card.appendChild(name);
+            const pokemon = await fetchPokemon(valor);
+            createCard(pokemon);
         } catch (error) {
-            const errorCatch = document.createElement("div");
-            errorCatch.classList.add("errorCard");
-            result?.appendChild(errorCatch);
-            const errorTextCatch = document.createElement("p");
-            errorTextCatch.classList.add("errorText");
-            errorTextCatch.innerHTML = "A\xf1ade un nombre valido";
-            errorCatch.appendChild(errorTextCatch);
+            createErrorCard("Introduce un nombre valido");
         } finally{
             removeSpinner();
         }
@@ -50,10 +31,33 @@ document.addEventListener("DOMContentLoaded", function() {
         spinner.classList.add("spinner");
         result?.appendChild(spinner);
     }
+    function removeSpinner() {
+        const spinner = document.querySelector(".spinner");
+        spinner?.remove();
+    }
+    function createErrorCard(errorMessage) {
+        const errorCard = document.createElement("div");
+        errorCard.classList.add("errorCard");
+        result?.appendChild(errorCard);
+        const errorText = document.createElement("p");
+        errorText.classList.add("errorText");
+        errorText.innerHTML = errorMessage;
+        errorCard.appendChild(errorText);
+    }
+    function createCard(info) {
+        const card = document.createElement("div");
+        card.classList.add("resultCard");
+        result?.appendChild(card);
+        debugger;
+        const img = document.createElement("img");
+        img.classList.add("cardImg");
+        img.src = info.sprites.front_default;
+        card.appendChild(img);
+        const name = document.createElement("p");
+        name.classList.add("cardName");
+        name.innerHTML = info.name;
+        card.appendChild(name);
+    }
 });
-function removeSpinner() {
-    const spinner = document.querySelector(".spinner");
-    spinner?.remove();
-}
 
 //# sourceMappingURL=index.242b51c6.js.map
--- typescript/proyectoPixabay/index.ts
+++ typescript/proyectoPixabay/index.ts
@@ -10,48 +10,29 @@ document.addEventListener("DOMContentLoaded", function () {
     };
   };
 
+  async function fetchPokemon(pokemonName: string): Promise<Pokemon> {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+
+    let response = await fetch(apiUrl);
+    let json = (await response.json()) as Pokemon;
+
+    return json;
+  }
+
   async function printResult() {
     let valor = busqueda.value;
 
     if (valor === "") {
-      const errorCard = document.createElement("div");
-      errorCard.classList.add("errorCard");
-      result?.appendChild(errorCard);
-
-      const errorText = document.createElement("p");
-      errorText.classList.add("errorText");
-      errorText.innerHTML = "Añade el nombre de un Pokemon";
-      errorCard.appendChild(errorText);
+      createErrorCard("Introduce un pokemon");
     } else {
       try {
         createSpinner();
-        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${valor}`;
 
-        let response = await fetch(apiUrl);
-        let json = (await response.json()) as Pokemon;
+        const pokemon = await fetchPokemon(valor);
 
-        const card = document.createElement("div");
-        card.classList.add("resultCard");
-        result?.appendChild(card);
-
-        const img = document.createElement("img");
-        img.classList.add("cardImg");
-        img.src = json.sprites.front_default;
-        card.appendChild(img);
-
-        const name = document.createElement("p");
-        name.classList.add("cardName");
-        name.innerHTML = json.name;
-        card.appendChild(name);
+        createCard(pokemon);
       } catch (error) {
-        const errorCatch = document.createElement("div");
-        errorCatch.classList.add("errorCard");
-        result?.appendChild(errorCatch);
-
-        const errorTextCatch = document.createElement("p");
-        errorTextCatch.classList.add("errorText");
-        errorTextCatch.innerHTML = "Añade un nombre valido";
-        errorCatch.appendChild(errorTextCatch);
+        createErrorCard("Introduce un nombre valido");
       } finally {
         removeSpinner();
       }
@@ -72,9 +53,37 @@ document.addEventListener("DOMContentLoaded", function () {
     spinner.classList.add("spinner");
     result?.appendChild(spinner);
   }
-});
 
-function removeSpinner() {
-  const spinner = document.querySelector(".spinner");
-  spinner?.remove();
-}
+  function removeSpinner() {
+    const spinner = document.querySelector(".spinner");
+    spinner?.remove();
+  }
+
+  function createErrorCard(errorMessage: string) {
+    const errorCard = document.createElement("div");
+    errorCard.classList.add("errorCard");
+    result?.appendChild(errorCard);
+
+    const errorText = document.createElement("p");
+    errorText.classList.add("errorText");
+    errorText.innerHTML = errorMessage;
+    errorCard.appendChild(errorText);
+  }
+
+  function createCard(info: Pokemon) {
+    const card = document.createElement("div");
+    card.classList.add("resultCard");
+    result?.appendChild(card);
+
+    debugger;
+    const img = document.createElement("img");
+    img.classList.add("cardImg");
+    img.src = info.sprites.front_default;
+    card.appendChild(img);
+
+    const name = document.createElement("p");
+    name.classList.add("cardName");
+    name.innerHTML = info.name;
+    card.appendChild(name);
+  }
+});
