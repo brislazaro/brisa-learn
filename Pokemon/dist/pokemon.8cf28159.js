f67663e2d63cfbc8803a246fefee2f1a93239d5e
brislazaro <brisa.lazaroc@gmail.com>
2024-01-15 18:42:14 +0100

    cambios

--- Pokemon/dist/pokemon.8cf28159.js
+++ Pokemon/dist/pokemon.8cf28159.js
@@ -1,24 +1,52 @@
 const body = document.querySelector(".body");
-const header = document.querySelector(".header-info");
+const pokemonID = document.querySelector(".header-info");
+const pokemonContainer = document.querySelector(".container");
 const searchParams = new URLSearchParams(window.location.search);
 const pokemonName = searchParams.get("name");
+var Type;
+(function(Type) {
+    Type["Grass"] = "grass";
+    Type["Fire"] = "fire";
+    Type["Water"] = "water";
+    Type["Bug"] = "bug";
+    Type["Normal"] = "normal";
+    Type["Poison"] = "poison";
+    Type["Electric"] = "electric";
+    Type["Fairy"] = "fairy";
+    Type["Ground"] = "ground";
+    Type["Ice"] = "ice";
+    Type["Fighting"] = "fighting";
+    Type["Flying"] = "flying";
+    Type["Psychic"] = "psychic";
+    Type["Rock"] = "rock";
+    Type["Ghost"] = "ghost";
+    Type["Steel"] = "steel";
+    Type["Dragon"] = "dragon";
+})(Type || (Type = {}));
 async function pokemonData(name) {
     const apiURL = `https://pokeapi.co/api/v2/pokemon/${name}`;
     let response = await fetch(apiURL);
-    let json = await response.json();
-    console.log(json);
-    headerInfo(json);
+    let pokemonInfo = await response.json();
+    console.log(pokemonInfo);
+    headerInfo(pokemonInfo);
+    printPokemon(pokemonInfo);
+    const types = pokemonInfo.types;
 }
 if (!pokemonName) alert("I need the pokemon name");
 else pokemonData(pokemonName);
-// function typeColor(name) {
-//   body?.classList.add("color");
-// }
 function headerInfo(info) {
     const paddedId = String(info.id).padStart(3, "0");
     const id = document.createElement("p");
     id.innerHTML = `#${paddedId}`;
-    header?.appendChild(id);
+    pokemonID?.appendChild(id);
+}
+function printPokemon(pokemon) {
+    const name = document.createElement("p");
+    name.innerHTML = pokemon.name;
+    pokemonContainer?.appendChild(name);
+    const type = document.createElement("p");
+    type.innerHTML = `${pokemon.id}`;
+    pokemonContainer?.appendChild(type);
 }
 
 //# sourceMappingURL=pokemon.8cf28159.js.map
--- Pokemon/pokemon.ts
+++ Pokemon/pokemon.ts
@@ -1,25 +1,58 @@
 const body = document.querySelector(".body");
-const header = document.querySelector(".header-info");
+const pokemonID = document.querySelector(".header-info");
+const pokemonContainer = document.querySelector(".container");
 
 const searchParams = new URLSearchParams(window.location.search);
 const pokemonName = searchParams.get("name");
+
+enum Type {
+  Grass = "grass",
+  Fire = "fire",
+  Water = "water",
+  Bug = "bug",
+  Normal = "normal",
+  Poison = "poison",
+  Electric = "electric",
+  Fairy = "fairy",
+  Ground = "ground",
+  Ice = "ice",
+  Fighting = "fighting",
+  Flying = "flying",
+  Psychic = "psychic",
+  Rock = "rock",
+  Ghost = "ghost",
+  Steel = "steel",
+  Dragon = "dragon",
+}
+
+type PokemonType = {
+  slot: number;
+  type: {
+    name: Type;
+    url: string;
+  };
+};
 
 type Pokemon = {
   name: string;
   id: number;
   height: number;
   weight: number;
+  types: PokemonType[];
 };
 
 async function pokemonData(name: string) {
   const apiURL = `https://pokeapi.co/api/v2/pokemon/${name}`;
 
   let response = await fetch(apiURL);
-  let json: Pokemon = await response.json();
+  let pokemonInfo: Pokemon = await response.json();
 
-  console.log(json);
+  console.log(pokemonInfo);
 
-  headerInfo(json);
+  headerInfo(pokemonInfo);
+  printPokemon(pokemonInfo);
+
+  const types = pokemonInfo.types;
 }
 
 if (!pokemonName) {
@@ -28,14 +61,20 @@ if (!pokemonName) {
   pokemonData(pokemonName);
 }
 
-// function typeColor(name) {
-//   body?.classList.add("color");
-// }
-
 function headerInfo(info: any) {
   const paddedId = String(info.id).padStart(3, "0");
   const id = document.createElement("p");
   id.innerHTML = `#${paddedId}`;
 
-  header?.appendChild(id);
+  pokemonID?.appendChild(id);
+}
+
+function printPokemon(pokemon: Pokemon) {
+  const name = document.createElement("p");
+  name.innerHTML = pokemon.name;
+  pokemonContainer?.appendChild(name);
+
+  const type = document.createElement("p");
+  type.innerHTML = `${pokemon.id}`;
+  pokemonContainer?.appendChild(type);
 }
