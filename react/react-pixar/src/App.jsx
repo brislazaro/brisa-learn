e12d2f46b2aece7c6cd0d5d52ffd0e5441a79ffa
brislazaro <brisa.lazaroc@gmail.com>
2024-01-24 17:50:25 +0100

    prhase and button

--- react/react-pixar/src/App.jsx
+++ react/react-pixar/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import logo from "./logopix.jpeg";
+import logo from "./img/logopix.jpeg";
 
 function App() {
   return (
@@ -19,7 +19,30 @@ function App() {
         </div>
       </header>
 
-      <main className="container"></main>
+      <main className="container">
+        <section className="principalImg">
+          <h1 className="text">RENDERMAN</h1>
+        </section>
+        <section className="prhase">
+          <div className="phrase-text">
+            <p>
+              “What began as an interesting artistic exercise is now an
+              indispensable tool. It's notable that twenty years after putting
+              its first pixels on the screen RenderMan remains the benchmark for
+              all rendering technology.”
+            </p>
+            <p className="phrase-autor">— GEORGE LUCAS</p>
+          </div>
+
+          <div className="button-container">
+            <hr className="separacion" />
+
+            <button className="button">VISIT RENDERMAN.PIXAR.COM</button>
+
+            <hr className="separacion" />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
