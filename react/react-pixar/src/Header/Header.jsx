7ced3288f66d94170ecf980b0bdf77a572444b71
brislazaro <brisa.lazaroc@gmail.com>
2024-01-26 11:38:40 +0100

    create header component

---
+++ react/react-pixar/src/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.css";
+import logo from "../img/logopix.jpeg";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logoContainer">
+        <img className="logo" src={logo}></img>
+      </div>
+      <div className="subTitle">
+        <p>FEATURE FILMS</p>
+        <p>SHORT FILMS</p>
+        <p>TECHNOLOGY</p>
+        <p>CARRERS</p>
+        <p>EXTRAS</p>
+        <p>ABOUT</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
