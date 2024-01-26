89cbbee814fc470a0e78762fbbbe3d4be8d048f0
brislazaro <brisa.lazaroc@gmail.com>
2024-01-26 11:44:31 +0100

    create imagen principal

---
+++ react/react-pixar/src/Principalimg/Principal.jsx
@@ -0,0 +1,11 @@
+import "./Principal.css";
+
+function Principal() {
+  return (
+    <section className="principalImg">
+      <h1 className="text">RENDERMAN</h1>
+    </section>
+  );
+}
+
+export default Principal;
